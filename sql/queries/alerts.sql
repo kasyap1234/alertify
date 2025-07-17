96d50ef29ffb5d6893f81d50d3aa5019dbce02fd
kasyap1234 <kasyap3103@gmail.com>
2025-07-17 16:58:56 +0530

    add alerts

--- sql/queries/alerts.sql
+++ sql/queries/alerts.sql
@@ -1,5 +1,14 @@
--- name: CreateAlert :one
+-- name: CreateAlert :exec
 INSERT INTO alerts (product_id,alert_message,alert_type,status) VALUES ($1,$2,$3,$4) RETURNING  *;
 
 -- name: ListAllAlerts :many
 SELECT * FROM alerts ORDER BY created_at DESC;
+
+-- name: GetPendingAlerts :many
+SELECT * FROM alerts WHERE status="pending" ORDER BY created_at DESC;
+
+-- name: UpdateAlert :exec
+UPDATE alerts SET status=$2 WHERE id=$1;
+
+-- name: GetAlertsByStatus :many
+SELECT * FROM alerts WHERE status=$1 ORDER BY created__at DESC;
