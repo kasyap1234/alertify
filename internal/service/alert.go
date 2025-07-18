d78c8314de20093efe6dfaf94c11ff51eac5b366
kasyap1234 <kasyap3103@gmail.com>
2025-07-18 17:08:41 +0530

    add alerts types

--- internal/service/alert.go
+++ internal/service/alert.go
@@ -69,3 +69,47 @@ func (s *AlertService) CreateAlert(ctx context.Context, product_id int32, alert_
 	}
 	return err
 }
+
+func (s *AlertService) GetAlertsByStatus(ctx context.Context, status string) ([]db.Alert, error) {
+	alerts, err := s.queries.GetAlertsByStatus(ctx, status)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get alert by status")
+		return nil, err
+	}
+	return alerts, nil
+}
+
+func (s *AlertService) GetAllAlerts(ctx context.Context) ([]db.Alert, error) {
+	alerts, err := s.queries.GetAllAlerts(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get all alerts ")
+		return nil, err
+	}
+	return alerts, nil
+}
+
+func (s *AlertService) GetPendingAlerts(ctx context.Context) ([]db.Alert, error) {
+	alerts, err := s.queries.GetPendingAlerts(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to get pending alerts ")
+		return nil, err
+	}
+	return alerts, nil
+}
+
+func (s *AlertService) UpdateAlert(ctx context.Context, id string, status string) error {
+	tx, err := s.db.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		log.Error().Err(err).Msg("update alert begin transaction failed ")
+		return err
+	}
+	defer tx.Rollback(ctx)
+	qtx := db.New(tx)
+	uuidID := uuid.Scan()
+	args := db.UpdateAlertParams{
+		ID:     id,
+		Status: status,
+	}
+	err := qtx.UpdateAlert(ctx, args)
+
+}
